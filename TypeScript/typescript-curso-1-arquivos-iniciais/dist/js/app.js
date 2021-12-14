e5fc9f8ca795de03a3658c4449402bccd0578dba
Mateus Roriz <mateusvroriz6b@gmail.com>
2021-12-13 22:27:00 -0300

    adicionando ambiente para desenvolvimento typescript

---
+++ TypeScript/typescript-curso-1-arquivos-iniciais/dist/js/app.js
@@ -0,0 +1,8 @@
+import { Negociacao } from "./models/negociacao.js";
+
+const negociacao = new Negociacao(new Date(), 10, 100);
+
+console.log(negociacao);
+console.log(negociacao.data);
+console.log(negociacao.quantidade);
+console.log(negociacao.volume);
