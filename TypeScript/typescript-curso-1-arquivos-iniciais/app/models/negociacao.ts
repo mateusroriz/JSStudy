09106c8a1603c97cc668b10dca6066841570bf50
Mateus Roriz <mateusvroriz6b@gmail.com>
2021-12-14 14:44:28 -0300

    configurando ambiente para converter typescript para javascript para rodar no browser

--- TypeScript/typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ TypeScript/typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -1,16 +1,25 @@
 export class Negociacao {
-    constructor(data, quantidade, valor) {
+    private _data;
+    private _quantidade;
+    private _valor; 
+
+    constructor(data, quantidade, valor){
         this._data = data;
         this._quantidade = quantidade;
         this._valor = valor;
     }
-    get data() {
+
+
+    get data(){
         return this._data;
     }
-    get quantidade() {
+
+    get quantidade(){
         return this._quantidade;
     }
-    get volume() {
+
+    get volume(){
         return this._quantidade * this._valor;
     }
+
 }
