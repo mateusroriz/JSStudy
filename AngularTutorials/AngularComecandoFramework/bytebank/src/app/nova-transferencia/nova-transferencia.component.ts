588d415f4a31cfd7fdf05169668db908631d6286
Mateus Roriz <mateusvroriz6b@gmail.com>
2022-01-03 22:45:01 -0300

    adicionando estilizacao para o formulario e criando evento no formulario para setar dados, usando prorpety bind para transmitir esses dados para o template/componente

--- AngularTutorials/AngularComecandoFramework/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ AngularTutorials/AngularComecandoFramework/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,17 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-nova-transferencia',
-    templateUrl:'./nova-transferencia.component.html',
-    styleUrls:['./nova-transferencia.component.scss']
-
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
-export class NovaTransferenciaComponent{
+export class NovaTransferenciaComponent {
+  valor: number;
+  destino: number;
 
+  transferir() {
+    console.log('solicitada nova transferencia');
+    console.log('Valor :', this.valor);
+    console.log('Destino :' , this.destino);
+  }
 }
