91f40b752c348d4595b965ad48567231749e94eb
Mateus Roriz <mateusvroriz6b@gmail.com>
2022-01-04 23:09:24 -0300

    exportando dados de transferencia com o decorator output e recebendo eles no componente e exibindo eles na tela por meio de interpolacao de dados

--- AngularTutorials/AngularComecandoFramework/bytebank/src/app/app.component.ts
+++ AngularTutorials/AngularComecandoFramework/bytebank/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencia: any;
+
+
+  transferir($event) {
+    console.log($event); //recebendo o valor do evento e dando console.log
+    this.transferencia = $event;
+  }
 }
--- AngularTutorials/AngularComecandoFramework/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ AngularTutorials/AngularComecandoFramework/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,12 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+
+  @Output() aoTransferir = new EventEmitter<any>(); //@Output eh decorator para compartilhar data entre componentes pais e filhos
+
   valor: number;
   destino: number;
 
   transferir() {
     console.log('solicitada nova transferencia');
-    console.log('Valor :', this.valor);
-    console.log('Destino :' , this.destino);
+    const valorEmitir = {valor: this.valor, destino: this.destino}
+    this.aoTransferir.emit(valorEmitir); //emitindo os valores por meio do @Output
+    this.limparCampos();
+  }
+
+  limparCampos(){
+    this.valor = null;
+    this.destino =null;
   }
 }
