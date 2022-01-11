218640eeb77402adfdd578b362b30bae2a8dc83a
Mateus Roriz <mateusvroriz6b@gmail.com>
2022-01-10 22:53:57 -0300

    adicionando modulo home e adicionando dentro dele os componentes home e login

--- AngularTutorials/AngularBoasPraticasArquiteturaeFormularios/gatitobook/src/app/home/home.module.ts
+++ AngularTutorials/AngularBoasPraticasArquiteturaeFormularios/gatitobook/src/app/home/home.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
-  ]
+  ],
+  exports: [HomeComponent],
 })
 export class HomeModule { }
