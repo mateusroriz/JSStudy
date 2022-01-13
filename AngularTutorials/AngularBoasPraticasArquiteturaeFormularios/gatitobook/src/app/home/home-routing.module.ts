704c12548791c2d8775c932b747daad213eb6170
Mateus Roriz <mateusvroriz6b@gmail.com>
2022-01-13 01:23:42 -0300

    adicionando componente services e interface para novo-usuario

--- AngularTutorials/AngularBoasPraticasArquiteturaeFormularios/gatitobook/src/app/home/home-routing.module.ts
+++ AngularTutorials/AngularBoasPraticasArquiteturaeFormularios/gatitobook/src/app/home/home-routing.module.ts
@@ -1,3 +1,4 @@
+import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
@@ -11,7 +12,11 @@ const routes: Routes = [
       {
         path: '',
         component: LoginComponent
-      }
+      },
+      {
+        path: 'novousuario',
+        component: NovoUsuarioComponent,
+      },
     ]
   },
 ];
---
+++ AngularTutorials/AngularBoasPraticasArquiteturaeFormularios/gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -0,0 +1,32 @@
+import { NovoUsuarioService } from './novo-usuario.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+
+@Component({
+  selector: 'app-novo-usuario',
+  templateUrl: './novo-usuario.component.html',
+  styleUrls: ['./novo-usuario.component.css'],
+})
+export class NovoUsuarioComponent implements OnInit {
+  novoUsuarioForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private NovoUsuarioService: NovoUsuarioService
+  ) {}
+
+  ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: [''],
+    }); //groupo eh um metodo factory
+  }
+
+  cadastrar(){
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log(novoUsuario);
+  }
+}
