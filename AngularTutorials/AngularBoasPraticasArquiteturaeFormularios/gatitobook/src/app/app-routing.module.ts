39bd14b68e2984900cf0d78d33fd73fd47ce9c31
Mateus Roriz <mateusvroriz6b@gmail.com>
2022-01-16 23:24:59 -0300

    fazendo template da grade de fotos de animais e utilizando rxjs para fazer o pipe dos observables e utilizando guarda de rotas para proteger urls de pagina que necessita de login

--- AngularTutorials/AngularBoasPraticasArquiteturaeFormularios/gatitobook/src/app/app-routing.module.ts
+++ AngularTutorials/AngularBoasPraticasArquiteturaeFormularios/gatitobook/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { LoginGuard } from './autenticacao/login.guard';
+import { AutenticacaoGuard } from './autenticacao/autenticacao.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -7,10 +9,12 @@ const routes: Routes = [
     path: 'home',
     loadChildren: () =>
       import('./home/home.module').then((module) => module.HomeModule),
+      canLoad: [LoginGuard]
   },
   {
     path: 'animais',
     loadChildren: () => import('./animais/animais.module').then((module) =>module.AnimaisModule),
+    canLoad:[AutenticacaoGuard]
   }
 ];
 
