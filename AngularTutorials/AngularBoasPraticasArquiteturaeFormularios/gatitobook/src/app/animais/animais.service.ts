68e0f4885226d65211b79a265acfabd48d36322d
Mateus Roriz <mateusvroriz6b@gmail.com>
2022-01-26 00:28:28 -0300

    adicionando componente para upload de uma nova foto  bem como um menu para navegacao para esses novos componentes

--- AngularTutorials/AngularBoasPraticasArquiteturaeFormularios/gatitobook/src/app/animais/animais.service.ts
+++ AngularTutorials/AngularBoasPraticasArquiteturaeFormularios/gatitobook/src/app/animais/animais.service.ts
@@ -1,7 +1,7 @@
 import { environment } from './../../environments/environment';
 import { TokenService } from './../autenticacao/token.service';
 import { Animais, Animal } from './animais';
-import { Observable, of ,throwError } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, mapTo } from 'rxjs/operators';
@@ -29,12 +29,24 @@ export class AnimaisService {
 
   curtir(id: number): Observable<boolean> {
     return this.http
-    .post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
+      .post(`${API}/photos/${id}/like`, {}, { observe: 'response' })
       .pipe(
         mapTo(true),
         catchError((error) => {
           return error.status === NOT_MODIFIED ? of(false) : throwError(error); //precisa retornar um observable que retorne false
         })
-      )
+      );
+  }
+
+  upload(descricao: string, permiteComentario: boolean, arquivo: File) {
+    const formData = new FormData();
+    formData.append('description', descricao);
+    formData.append('allowComments', permiteComentario ? 'true' : 'false');
+    formData.append('imageFile', arquivo);
+
+    return this.http.post(`${API}/photos/upload`, formData, { //isso eh um observable
+      observe: 'events',
+      reportProgress: true,
+    });
   }
 }
