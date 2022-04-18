3f43ed5774479b51ad26ce93d3d24a13456ac38b
Mateus Roriz <mateusvroriz6b@gmail.com>
2022-04-18 18:03:07 -0300

    adicionar componentes novos para testar o CLI e reforcar memorizacao das estruturas

--- AngularTutorials/CompleteGuideAngularUdemy/my-first-app/src/app/app.module.ts
+++ AngularTutorials/CompleteGuideAngularUdemy/my-first-app/src/app/app.module.ts
@@ -1,14 +1,23 @@
+import { ServerComponent } from './server/server.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SucessAlertComponent } from './sucess-alert/sucess-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SucessAlertComponent
   ],
   imports: [
     BrowserModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
