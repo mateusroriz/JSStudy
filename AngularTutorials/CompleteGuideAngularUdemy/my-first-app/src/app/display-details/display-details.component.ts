b9f4fe7dab2a2698bf81d36daa7c95b1e41f3bd5
Mateus Roriz <mateusvroriz6b@gmail.com>
2022-06-06 22:58:13 -0300

    adding base project from recipes web page

--- AngularTutorials/CompleteGuideAngularUdemy/my-first-app/src/app/display-details/display-details.component.ts
+++ AngularTutorials/CompleteGuideAngularUdemy/my-first-app/src/app/display-details/display-details.component.ts
@@ -8,7 +8,6 @@ import { Component, OnInit } from '@angular/core';
 export class DisplayDetailsComponent implements OnInit {
   visible = false;
   arrayNumber = [];
-  clicksNumber = 0;
   constructor() {}
 
   ngOnInit(): void {}
