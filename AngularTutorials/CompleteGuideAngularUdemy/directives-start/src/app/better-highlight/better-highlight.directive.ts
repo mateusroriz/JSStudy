f7768104a76af0b176391af69cc7ca8beb65674a
Mateus Roriz <mateusvroriz6b@gmail.com>
2022-09-15 23:48:08 -0300

    adicionando diretivas customizadas e usando ngswitch para renderizar elementos na DOM

--- AngularTutorials/CompleteGuideAngularUdemy/directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ AngularTutorials/CompleteGuideAngularUdemy/directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,15 +1,29 @@
-import { ElementRef } from '@angular/core';
+import { ElementRef, HostBinding, HostListener, Input } from '@angular/core';
 import { Directive, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input('appBetterHighlight') highlightColor: string = 'blue';
+  @HostBinding('style.backgroundColor') backgroundColor: string;
 
   constructor(private elemRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elemRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.defaultColor;
+    // this.renderer.setStyle(this.elemRef.nativeElement, 'background-color', 'blue');
   }
+
+  @HostListener('mouseenter') mouseOver(eventData: Event){
+    // this.renderer.setStyle(this.elemRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.highlightColor;
+  }
+  @HostListener('mouseleave') mouseLeave(eventData: Event){
+    // this.renderer.setStyle(this.elemRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.defaultColor;
+  }
+
 
 }
