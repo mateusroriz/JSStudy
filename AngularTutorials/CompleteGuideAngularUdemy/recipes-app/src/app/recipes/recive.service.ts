0301add010aa9545138a0eba06cd993206ae08dc
Mateus Roriz <mateusvroriz6b@gmail.com>
2023-01-27 17:40:51 -0300

    adicionar novas funcoes para os servicos de receita e lista de compras para controlar ingredientes entre modulos

--- AngularTutorials/CompleteGuideAngularUdemy/recipes-app/src/app/recipes/recive.service.ts
+++ AngularTutorials/CompleteGuideAngularUdemy/recipes-app/src/app/recipes/recive.service.ts
@@ -1,5 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { Ingredient } from './../shared/ingredient.model';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from "./recipe.model";
+
+@Injectable()
 
 export class RecipeService{
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,16 +12,28 @@ export class RecipeService{
     new Recipe(
       'A test Recipe',
       'this is simply a description',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg'
-    ),
+      'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg',
+      [new Ingredient('ingredient 1', 1),
+      new Ingredient('ingredient 2', 2)
+      ]),
     new Recipe(
       'A test Recipe 2',
       'this is simply a description 2',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg'
-    ),
+      'https://images.immediate.co.uk/production/volatile/sites/30/2013/05/Puttanesca-fd5810c.jpg',
+      [new Ingredient('ingredient 3', 3),
+      new Ingredient('ingredient 4', 4)
+      ]),
   ];
+
+  constructor(private shoppingListService: ShoppingListService){
+
+  }
 
   getRecipes(){
     return this.recipes.slice(); //returns a copy of the array instead of the array itself
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]){
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
--- AngularTutorials/CompleteGuideAngularUdemy/recipes-app/src/app/shopping-list/shopping-list.service.ts
+++ AngularTutorials/CompleteGuideAngularUdemy/recipes-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,28 @@
-export class ShoppingListService{
+import { Ingredient } from './../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
 
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  private  ingredients : Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  getIngredients(){
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient){
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice()); //passando a copia da lista de ingredientes atualizada
+  }
+
+  addIngredients(ingredients: Ingredient[]){
+    //for(let ingredient of ingredients){
+    //  this.addIngredient(ingredient);
+    //  } valido mas emite demais
+    this.ingredients.push(...ingredients); //usando o ... spread operator para transformar um array de elementos em uma lista de elementos
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
