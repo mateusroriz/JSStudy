928105da30b2c0d8d7000ec44fd0a59d2e1d6a36
Mateus Roriz <mateusvroriz6b@gmail.com>
2023-01-30 18:05:53 -0300

    adicionar novas formas de navegacao entre rotas com routerlink e como passar parametros entre essas rotas

--- AngularTutorials/CompleteGuideAngularUdemy/routing-start/src/app/users/user/user.component.ts
+++ AngularTutorials/CompleteGuideAngularUdemy/routing-start/src/app/users/user/user.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute, Params } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute ) { }
 
   ngOnInit() {
+    this.user = {
+    id: this.activeRoute.snapshot.params['id'], //snapshot so carrega na primeira vez que o arquivo eh acessado
+    name: this.activeRoute.snapshot.params['name']
+    }
+    this.activeRoute.params //params is observable // params esta sendo inscrito quando o ngOnInit roda
+    .subscribe((params: Params)=>{ // mudando a informacao do usuario sempre que a mesma for mudada na rota
+      this.user.id = params['id'];
+      this.user.name = params['name']
+    });
   }
 
 }
