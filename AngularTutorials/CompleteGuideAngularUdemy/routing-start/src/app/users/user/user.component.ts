99c0b1ff4487aad8f5fdda6e886f4049d34e9b67
Mateus Roriz <mateusvroriz6b@gmail.com>
2023-01-31 18:03:51 -0300

    adicionar onDestroy para o subscribe de params e passando queryparams e fragments na url

--- AngularTutorials/CompleteGuideAngularUdemy/routing-start/src/app/users/user/user.component.ts
+++ AngularTutorials/CompleteGuideAngularUdemy/routing-start/src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
 import { ActivatedRoute, Params } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubscription: Subscription;
 
   constructor(private activeRoute: ActivatedRoute ) { }
 
@@ -16,11 +18,15 @@ export class UserComponent implements OnInit {
     id: this.activeRoute.snapshot.params['id'], //snapshot so carrega na primeira vez que o arquivo eh acessado
     name: this.activeRoute.snapshot.params['name']
     }
-    this.activeRoute.params //params is observable // params esta sendo inscrito quando o ngOnInit roda
+   this.paramsSubscription =  this.activeRoute.params //params is observable // params esta sendo inscrito quando o ngOnInit roda
     .subscribe((params: Params)=>{ // mudando a informacao do usuario sempre que a mesma for mudada na rota
       this.user.id = params['id'];
       this.user.name = params['name']
     });
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription.unsubscribe() // nao eh necessario, o angular faz isso pra vc, mas eh importante fazer
   }
 
 }
